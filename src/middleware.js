066274320d4082a8fbb60331883122110fd5e84c
Bruno Camargo <eusoubrunocamargo@gmail.com>
2023-06-28 15:55:24 -0300

    starting app with auth

---
+++ src/middleware.js
@@ -0,0 +1,27 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
+
+/**
+ * Any Server Component route that uses a Supabase client must be added to this
+ * middleware's `matcher` array. Without this, the Server Component may try to make a
+ * request to Supabase with an expired `access_token`.
+ */
+export async function middleware(req) {
+  const res = NextResponse.next();
+
+  const supabase = createMiddlewareClient({ req, res });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    return NextResponse.redirect('http://localhost:3000/');
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ['/dashboard'],
+};
---
+++ src/pages/dashboard.js
@@ -0,0 +1,13 @@
+import styles from '@/styles/Dashboard.module.css'
+
+export default function Dashboard() {
+
+
+    return (
+        <>
+            <main className={styles.mainContainer}>
+                <span>Rota protegida</span>
+            </main>
+        </>
+    )
+}
